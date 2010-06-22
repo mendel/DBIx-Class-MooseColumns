c6b989723d6214a97f62d194be31868985e34676
Norbert Buchmuller <norbi@nix.hu>
2010-06-22 16:44:16 +0200

    Release 0.10.

--- lib/DBIx/Class/MooseColumns.pm
+++ lib/DBIx/Class/MooseColumns.pm
@@ -30,11 +30,11 @@ DBIx::Class::MooseColumns - Lets you write DBIC add_column() definitions as attr
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 
 =head1 SYNOPSIS
