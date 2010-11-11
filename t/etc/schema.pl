d65d47f3b08a96f5f8b1c9018a1bf41a6231da5b
Norbert Buchmuller <norbi@nix.hu>
2010-11-11 17:04:12 +0100

    Fixed a bug in the fixture.
    It seems a different SQLite/SQL::Translator version assumes the column
    is not nullable by default.

--- t/etc/schema.pl
+++ t/etc/schema.pl
@@ -4,7 +4,7 @@
     basic => {
       'Artist'  => [
         [ 'artist_id', 'name', 'title', 'phone',  'address',      'birthday',   'initials', 'is_active', 'favourite_color' ],
-        [ 1,           'foo',  'Dr',    '123456', 'Some where 1', '2010-06-21', 'DF',       0,           undef,            ],
+        [ 1,           'foo',  'Dr',    '123456', 'Some where 1', '2010-06-21', 'DF',       0,           'GREEN'           ],
       ],
     },
   },
