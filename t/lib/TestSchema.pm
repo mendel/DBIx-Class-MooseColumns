939da1df36a3685007c65da77d9a82e4a4ef18c7
Norbert Buchmuller <norbi@nix.hu>
2011-01-27 12:10:04 +0100

    Workaround to make it work with new (1.21) Moose.

--- t/lib/TestSchema.pm
+++ t/lib/TestSchema.pm
@@ -6,6 +6,9 @@ use namespace::autoclean;
 
 extends 'DBIx::Class::Schema';
 
+# just to make MooseX::NonMoose happy
+sub new { }
+
 __PACKAGE__->load_namespaces;
 
 __PACKAGE__->meta->make_immutable;
