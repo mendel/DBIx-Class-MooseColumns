20ebf9d5f859fe65961e88743891559c648e1689
Norbert Buchmuller <norbi@nix.hu>
2011-03-15 13:16:57 +0100

    Removed workaround of a MooseX::NonMoose weirdness.
    Now TestSchema does not need a dummy constructor (which, btw, made
    01-warnings.t fail).

--- t/lib/TestSchema.pm
+++ t/lib/TestSchema.pm
@@ -6,9 +6,6 @@ use namespace::autoclean;
 
 extends 'DBIx::Class::Schema';
 
-# just to make MooseX::NonMoose happy
-sub new { }
-
 __PACKAGE__->load_namespaces;
 
 __PACKAGE__->meta->make_immutable;
