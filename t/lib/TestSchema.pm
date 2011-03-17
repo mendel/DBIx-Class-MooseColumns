9588569fa6c106528ae7ad53773d4477207f48e0
Norbert Buchmuller <norbi@nix.hu>
2011-03-17 18:31:53 +0100

    Fixed a test failure with recent DBIx::Class.
    TestSchema is not a MooseX::NonMoose class any more: Its constructor is
    named "connect" and not "new", which MooseX::NonMoose does not support
    (and emits a warning that breaks some tests).

--- t/lib/TestSchema.pm
+++ t/lib/TestSchema.pm
@@ -1,13 +1,12 @@
 package TestSchema;
 
 use Moose;
-use MooseX::NonMoose;
 use namespace::autoclean;
 
 extends 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces;
 
-__PACKAGE__->meta->make_immutable;
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 
 1;
